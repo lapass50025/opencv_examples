fa6a66d84c9ccb72952b179a13f0d2c05b7c498f
lapass50025 <lapss50025@gmail.com>
2020-12-13 23:01:01 +0900

    modified adapativeThreshold

--- 16_adaptiveThreshold.py
+++ 16_adaptiveThreshold.py
@@ -18,15 +18,20 @@ img_mean = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_B
 img_gaussian = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 4)
 
 # 출력하기
+
+# 원본 파일
 plt.subplot(1, 4, 1)
 plt.imshow(img, cmap='gray')
 
+# threshold binary
 plt.subplot(1, 4, 2)
 plt.imshow(img_threshold, cmap='gray')
 
+# adaptive threshold by mean
 plt.subplot(1, 4, 3)
 plt.imshow(img_mean, cmap='gray')
 
+# adaptive threshold by gaussian
 plt.subplot(1, 4, 4)
 plt.imshow(img_gaussian, cmap='gray')
 
